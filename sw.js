185e4fddef69199602a8c2e39a70391c682c25ea
maoyaxu2000-beep <maoyaxu2000@gmail.com>
2026-06-15 19:28:14 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+/* 离线缓存 service worker */
+const CACHE = 'wuban-english-v1';
+const ASSETS = ['./','./index.html','./app.js','./data.js','./icon.svg','./manifest.webmanifest'];
+self.addEventListener('install', e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request).then(resp=>{
+      const copy=resp.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+      return resp;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
